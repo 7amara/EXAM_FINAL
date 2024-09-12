bdad0fd746735f825676880217b8f32032a89ee0
O23433 <O23433@lab.aca>
2024-09-11 21:18:51 -0500

    exam

---
+++ Preg02.js
@@ -0,0 +1,6 @@
+function myfunction(peso,altura)
+    let peso;
+    let altura;
+    let IMC;
+    IMC= document.getElementById(peso)/(document.getElementById(altura) ) *(document.getElementById(altura) );
+    console.log("EL IMC ES : "+ IMC)
